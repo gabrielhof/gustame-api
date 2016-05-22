577473c02637797801a93e51e6bb2051aceea9f3
Gabriel Hoff <gabrielhof@gmail.com>
2016-05-22 14:41:17 -0300

    Finalizadas as integraçoes com o mongo db

--- routes/routes.js
+++ routes/routes.js
@@ -8,57 +8,32 @@ var connection = null;
 
 mongoClient.connect(process.env.MONGO_URL || "mongodb://localhost:27017/gustame", function(error, db) {
   connection = db;
-  
-  connection.createCollection("pratos");
-  connection.createCollection("pedidos");
 });
 
-router.get('/pratos', function(req, res, next) {
+router.get('/pratos/', function(req, res, next) {
+  var collection = connection.collection("pratos");
 
-  var pratos = connection.collection("pratos");
-
-  pratos.find().toArray(function(data) {
-    console.log(data);
-
-    res.json([
-      {
-        id: 1,
-        nome: "Pudim",
-        descricao: "Delicioso pudim!",
-        imagemUrl: "http://s.glbimg.com/po/rc/media/2015/11/10/12_07_13_265_pudim_de_leite_condensado_4.jpg",
-        cozinheiro: {
-          nome: "Gabriel Hoff",
-          cidade: "Novo Hamburgo",
-          imagemUrl: "http://www.gravatar.com/avatar/39ea753c11ef1d94e09c1a9265767b4a"
-        }
-      }
-    ]);
+  collection.find().toArray(function(error, pratos) {
+    res.json(pratos);
   });
 });
 
 router.get('/pratos/:id', function(req, res, next) {
-  var pratos = connection.createCollection("pratos");
+  var collection = connection.collection("pratos");
 
-  pratos.find({"_id": ObjectId(req.params.id)}).toArray(function(data) {
-    console.log(data);
-
-    res.json({
-      id: 1,
-      nome: "Pudim",
-      descricao: "Delicioso pudim!",
-      imagemUrl: "http://s.glbimg.com/po/rc/media/2015/11/10/12_07_13_265_pudim_de_leite_condensado_4.jpg",
-      cozinheiro: {
-        nome: "Gabriel Hoff",
-        cidade: "Novo Hamburgo",
-        imagemUrl: "http://www.gravatar.com/avatar/39ea753c11ef1d94e09c1a9265767b4a"
-      }
-    });
+  collection.findOne({"_id": ObjectId(req.params.id)}, function(error, prato) {
+    res.json(prato);
   });
 });
 
 router.post('/pedidos', function(req, res, next) {
   var pedido = req.body;
-  res.json(pedido);
+
+  var collection = connection.collection("pedidos");
+
+  collection.insert(pedido, function() {
+    res.json(pedido);
+  });
 });
 
 module.exports = router;
